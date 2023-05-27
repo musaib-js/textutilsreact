9ff052d38e2bb6bbcb0e608d66339d19887373c0
musaib-js <musaibaltafurahman@gmail.com>
2023-05-27 22:42:58 +0530

    title change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar(props) {
     <div>
        <nav className="navbar navbar-expand-lg bg-body-secondary" data-bs-theme={`${props.mode}`}>
     <div className="container-fluid">
-      <a className="navbar-brand" href="/">TextUtils</a>
+      <a className="navbar-brand" href="/">Textify</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
